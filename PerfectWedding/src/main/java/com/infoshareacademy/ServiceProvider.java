50b99d96243f2b6985d70c56e36c588798fb5909
dacresillvaant <mateusz.kubis94@gmail.com>
2021-10-06 16:59:16 +0200

    Added ServiceProvider class

---
+++ PerfectWedding/src/main/java/com/infoshareacademy/ServiceProvider.java
@@ -0,0 +1,19 @@
+package com.infoshareacademy;
+
+import java.util.Date;
+
+public class ServiceProvider {
+
+    private int ID;
+    private String companyName;
+    private String ownerName;
+    private String ownerSurname;
+    private int phone;
+    private String email;
+    private String websiteAddress;
+    private Location location;
+    private ServiceType serviceType;
+    private Date[] availability;
+    private boolean isActive;
+    private int rating;
+}
