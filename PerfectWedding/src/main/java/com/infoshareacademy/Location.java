9d7917876181d2fd81530fa729cc59d644317517
dacresillvaant <mateusz.kubis94@gmail.com>
2021-10-06 16:57:10 +0200

    Added Location class

---
+++ PerfectWedding/src/main/java/com/infoshareacademy/Location.java
@@ -0,0 +1,12 @@
+package com.infoshareacademy;
+
+public class Location {
+    private String country;
+    private String voivodeship;
+    private String county;
+    private String commune;
+    private String locality;
+    private String postalCode;
+    private String streetName;
+    private int buildingNumber;
+}
