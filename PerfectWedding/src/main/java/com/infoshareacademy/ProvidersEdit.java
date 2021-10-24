4afb625885a4ed1e5449f8375fd01cde1bb20f4b
dacresillvaant <mateusz.kubis94@gmail.com>
2021-10-24 18:32:24 +0200

    ProvidersEdit - added editSelectedProvider method

--- PerfectWedding/src/main/java/com/infoshareacademy/ProvidersEdit.java
+++ PerfectWedding/src/main/java/com/infoshareacademy/ProvidersEdit.java
@@ -1,9 +1,13 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.model.Location;
 import com.infoshareacademy.model.ServiceProvider;
+import com.infoshareacademy.model.ServiceType;
 
 import java.util.List;
 import java.util.Scanner;
+
+import static com.infoshareacademy.PerfectWeddingUtils.scanInput;
 
 public class ProvidersEdit {
     public void editProvider() {
@@ -16,7 +20,50 @@ public class ProvidersEdit {
         }
         System.out.println("Chose provider to be edited (type ID): ");
         int chosenProviderId = scanner.nextInt();
-//        editSelectedProvider(chosenProviderId-1);
+        editSelectedProvider(chosenProviderId-1);
+    }
+
+    public void editSelectedProvider (int i) {
+        List<ServiceProvider> providersList = App.providerDataBase.getListOfProviders();
+        Scanner scanner = new Scanner(System.in);
+        boolean areYouFinished = true;
+        System.out.println("Chosen provider details are: ");
+        System.out.println(providersList.get(i).toString());
+
+        do {
+            String chosenField = null;
+            chosenField = scanInput("Which field would you like to edit?");
+
+            switch (chosenField) {
+                case "companyName": providersList.get(i).setCompanyName(scanInput("Podaj nazwe firmy: "));
+                    break;
+                case "ownerName": providersList.get(i).setOwnerName(scanInput("Podaj imie wlasciciela: "));
+                    break;
+                case "ownerSurname": providersList.get(i).setOwnerSurname(scanInput("Podaj nazwisko wlasciciela: "));
+                    break;
+                case "phone": providersList.get(i).setPhone(scanInput("Podaj telefon: "));
+                    break;
+                case "email": providersList.get(i).setEmail(scanInput("Podaj imie email: "));
+                    break;
+                case "websiteAddress": providersList.get(i).setWebsiteAddress(scanInput("Podaj strone internetowa: "));
+                    break;
+                case "location": providersList.get(i).setLocation(new Location(scanInput("Podaj miejscowosc: ")));
+                    break;
+                case "service": providersList.get(i).setServiceType(new ServiceType(scanInput("Podaj rodzaj uslugi: ")));
+                    break;
+                default:
+                    System.out.println("None field chosen");
+                    editProvider();
+
+            }
+            String response = scanInput("Would you like to change any other field? (yes/no)");
+            if (response.equals("yes")) {
+                areYouFinished = true;
+            } else {
+                areYouFinished = false;
+            }
+        }
+        while (areYouFinished);
     }
 
 }
