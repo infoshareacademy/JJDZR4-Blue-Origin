77124d13de915b015307f082d881a9efc2fa90ec
dacresillvaant <mateusz.kubis94@gmail.com>
2021-10-24 18:28:07 +0200

    ProvidersEdit - added editProvider method

--- PerfectWedding/src/main/java/com/infoshareacademy/ProvidersEdit.java
+++ PerfectWedding/src/main/java/com/infoshareacademy/ProvidersEdit.java
@@ -1,7 +1,22 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.model.ServiceProvider;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class ProvidersEdit {
     public void editProvider() {
+        List<ServiceProvider> providersList = App.providerDataBase.getListOfProviders();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Current providers are shown below: ");
+        for (ServiceProvider n: providersList) {
+            System.out.println("ID " + n.getID() + " and owner name is " + n.getOwnerName());
+        }
+        System.out.println("Chose provider to be edited (type ID): ");
+        int chosenProviderId = scanner.nextInt();
+//        editSelectedProvider(chosenProviderId-1);
     }
 
 }
