c296ac1fca16540aea4a9cfbedc2b3a98fde1424
PRZWAN <wanagos@interia.eu>
2021-10-26 19:08:26 +0200

    Provider Disable class populated with method DeleteProvider (). This method is 90% the same with method EditProvider in class ProviderEdit - @ Mateusz - maybe we should extract both of them and put in utils.

--- PerfectWedding/src/main/java/com/infoshareacademy/ProvidersDisable.java
+++ PerfectWedding/src/main/java/com/infoshareacademy/ProvidersDisable.java
@@ -1,7 +1,23 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.model.ServiceProvider;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static com.infoshareacademy.PerfectWeddingUtils.scanInput;
+
 public class ProvidersDisable {
 
+    List<ServiceProvider> providersList = App.providerDataBase.getListOfProviders();
     public void deleteProvider() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Aktualna lista usługodawców wygląda następująco: ");
+        for (ServiceProvider n : providersList) {
+            System.out.println("ID " + n.getID() + " Nazwa firmy " + n.getCompanyName() + " Imię i nazwisko właściciela " + n.getOwnerName() + " " + n.getOwnerSurname());
+        }
+        int chosenProviderId = scanInput("Wybierz usługodawcę, którego chcesz edytować (podaj ID): ", 1, providersList.size());
+        providersList.get(chosenProviderId - 1).setActive(false);
     }
+
 }
