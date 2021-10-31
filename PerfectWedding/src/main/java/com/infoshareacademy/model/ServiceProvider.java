bc9733bd85a2cafcd42634cec3172deea0daa819
PRZWAN <wanagos@interia.eu>
2021-10-31 22:09:31 +0100

    pierwsz proba

--- PerfectWedding/src/main/java/com/infoshareacademy/model/ServiceProvider.java
+++ PerfectWedding/src/main/java/com/infoshareacademy/model/ServiceProvider.java
@@ -1,7 +1,10 @@
 package com.infoshareacademy.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.ArrayList;
 import java.util.List;
+@JsonIgnoreProperties(ignoreUnknown = true)
 
 public class ServiceProvider {
     private int id;
