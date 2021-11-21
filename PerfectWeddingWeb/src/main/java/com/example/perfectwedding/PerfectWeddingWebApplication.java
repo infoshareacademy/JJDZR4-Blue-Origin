917458000572f9439198002ec9abdd21cd51c4b5
dacresillvaant <mateusz.kubis94@gmail.com>
2021-11-21 12:53:24 +0100

    Initial commit with Spring

---
+++ PerfectWeddingWeb/src/main/java/com/example/perfectwedding/PerfectWeddingWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.perfectwedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectWeddingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfectWeddingWebApplication.class, args);
+    }
+
+}
