2f56b860a5ba761a2b2060c08a35e2978119b000
dacresillvaant <mateusz.kubis94@gmail.com>
2022-01-09 12:51:05 +0100

    Changed old bindings to previous service to new service.

--- PerfectWeddingWeb/src/main/java/com/infoshareacademy/controllers/ProviderController.java
+++ PerfectWeddingWeb/src/main/java/com/infoshareacademy/controllers/ProviderController.java
@@ -1,45 +1,46 @@
 package com.infoshareacademy.controllers;
 
+import com.infoshareacademy.services.ServiceProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProviderController {
 
-//    @Autowired
-//    ServiceProvider serviceProvider;
-//
-//    @GetMapping("providers/create")
-//
-//    public String mainPage() {
-//        return "ProviderAdd";
-//    }
-//
-//    @GetMapping("providers/edit")
-//    @ResponseBody
-//    public String clientsPage() {
-//        return "tu bedzie froamtka do edycji uslugodawcy";
-//    }
-//
-//    @GetMapping("providers/deactivate")
-//    @ResponseBody
-//    public String providersPage() {
-//        return "tu bedzie froamtka do dezaktywacji uslugodawcy";
-//
-//    }
-//
-//
-//    //Klasa ServceProvider w package repository jest wzorcem, ktorego nie powinnismy ruszac.
-//    //Jest ona odwzorowana w SeriveProviderDto, tkory jest w package dto. Obiekt tej klasy jest przekazywany w metodzie Post
-//    @PostMapping(value = "providers/addvalue")
-//    @ResponseBody
-//    public String create(ServiceProviderDto serviceProviderDto) {
-//        serviceProvider.setCompanyName(serviceProviderDto.getCompanyName());
-//        serviceProvider.setOwnerName(serviceProviderDto.getOwnerName());
-//        return serviceProvider.toString();
-//    }
 
+    private ServiceProviderService serviceProviderService;
 
+    @Autowired
+    public ProviderController(ServiceProviderService serviceProviderService) {
+        this.serviceProviderService = serviceProviderService;
+    }
 
+    @GetMapping("providers/create")
 
+    public String mainPage() {
+        return "ProviderAdd";
+    }
+
+    @GetMapping("providers/edit")
+    @ResponseBody
+    public String clientsPage() {
+        return "tu bedzie froamtka do edycji uslugodawcy";
+    }
+
+    @GetMapping("providers/deactivate")
+    @ResponseBody
+    public String providersPage() {
+        return "tu bedzie froamtka do dezaktywacji uslugodawcy";
+    }
+
+    @RequestMapping(value = "/all-providers")
+    public String showAllProviders(Model model) {
+        model.addAttribute("allProvidersTH", serviceProviderService.returnAllServiceProviders());
+        return "AllProviders";
+    }
 
 }
