b65b5d940a0059fd3d4acb3d32296c923fef94d5
dacresillvaant <mateusz.kubis94@gmail.com>
2022-01-22 19:00:11 +0100

    Added search by city.

--- PerfectWeddingWeb/src/main/java/com/infoshareacademy/services/ServiceProviderService.java
+++ PerfectWeddingWeb/src/main/java/com/infoshareacademy/services/ServiceProviderService.java
@@ -2,13 +2,18 @@ package com.infoshareacademy.services;
 
 import com.infoshareacademy.domain.ServiceProvider;
 import com.infoshareacademy.dto.ServiceAddProviderDto;
+import com.infoshareacademy.dto.ServiceProviderDto;
+import com.infoshareacademy.dto.ServiceProviderSearchDto;
 import com.infoshareacademy.mapper.ServiceProviderMapper;
 import com.infoshareacademy.repository.ServiceProviderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.util.StringUtils;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceProviderService {
@@ -38,5 +43,19 @@ public class ServiceProviderService {
     public void addProvider(ServiceAddProviderDto serviceAddProviderDto) {
         serviceProviderRepo.getServiceProvidersList()
                 .add(serviceProviderMapper.mapperFromAddDto(serviceAddProviderDto));
+    }
+
+//    public List<ServiceProvider> findByCity(String city) {
+//        return serviceProviderRepo.getServiceProvidersList()
+//                .stream()
+//                .filter(serviceProvider -> serviceProvider.getLocation().getCity().contains(city))
+//                .collect(Collectors.toList());
+//    }
+
+    public List<ServiceProvider> findByCity(String city) {
+        return serviceProviderRepo.getServiceProvidersList()
+                .stream()
+                .filter(serviceProvider -> StringUtils.containsIgnoreCase(serviceProvider.getLocation().getCity(), city, Locale.ROOT))
+                .collect(Collectors.toList());
     }
 }
