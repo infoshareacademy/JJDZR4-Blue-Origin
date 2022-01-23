7eb5b75cc18ca58f7ceda202d849fc0aad168a06
dacresillvaant <mateusz.kubis94@gmail.com>
2022-01-23 16:05:03 +0100

    Moved /clients controler to homepage controller.

--- PerfectWeddingWeb/src/main/java/com/infoshareacademy/controllers/HomePageController.java
+++ PerfectWeddingWeb/src/main/java/com/infoshareacademy/controllers/HomePageController.java
@@ -1,6 +1,8 @@
 package com.infoshareacademy.controllers;
 
+import com.infoshareacademy.dto.ServiceSearchProviderDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -14,6 +16,12 @@ public class HomePageController {
     @GetMapping("/providers")
     public String providersPage() {
         return "ProviderMenu";
+    }
+
+    @GetMapping("/clients")
+    public String findByCity(Model model) {
+        model.addAttribute("cityAndTypeOfService", new ServiceSearchProviderDto());
+        return "ClientMenu";
     }
 
 }
