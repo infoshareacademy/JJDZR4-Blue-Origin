e6dc4ed672a28d972571728feba7690157e4d4b2
PawelDampc <dampc86pawel@gmail.com>
2022-02-13 13:04:52 +0100

    create location table

--- PerfectWeddingWeb/src/main/java/com/infoshareacademy/domain/Location.java
+++ PerfectWeddingWeb/src/main/java/com/infoshareacademy/domain/Location.java
@@ -1,18 +1,34 @@
 package com.infoshareacademy.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
 @Getter
 @Setter
+@Table(name = Location.TABLE_NAME)
 public class Location {
 
-    private Voivodeship voivodeship;
-    private String city;
-    public Location() {
-    }
+    public static final String TABLE_NAME = "location";
+    public static final String COLUMN_PREFIX = "l_";
 
-    public Location( String city, Voivodeship voivodeship) {
+    @Id
+    @GeneratedValue
+    @Column(name = COLUMN_PREFIX + "id")
+    private int id;
+
+    @Column(name = COLUMN_PREFIX + "voivodeship", nullable = false)
+    private Voivodeship voivodeship;
+
+    @Column(name = COLUMN_PREFIX + "city", nullable = false)
+    private String city;
+
+
+    public Location(String city, Voivodeship voivodeship) {
         this.voivodeship = voivodeship;
         this.city = city;
     }
