b5ecf8b9b09ef4524685e2b3d95d3a7fd7037b70
PRZWAN <wanagos@interia.eu>
2022-03-10 13:58:37 +0100

    after provider is rate, only rated provider is shown.

--- PerfectWeddingWeb/src/main/java/com/infoshareacademy/controllers/ClientController.java
+++ PerfectWeddingWeb/src/main/java/com/infoshareacademy/controllers/ClientController.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.controllers;
 
+import com.infoshareacademy.domain.ServiceProvider;
 import com.infoshareacademy.dto.ServiceSearchProviderDto;
 import com.infoshareacademy.mapper.ServiceProviderMapper;
 import com.infoshareacademy.services.ServiceProviderService;
@@ -7,7 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 
@@ -27,6 +33,16 @@ public class ClientController {
         modelOfFoundProviders
                 .addAttribute("providersByServiceTH", serviceProviderService.findProviders(serviceSearchProviderDto.getServiceType(), serviceSearchProviderDto.getCity(), serviceSearchProviderDto.getDate(), true))
                 .addAttribute("toggleDeactivateEdit", "rate");
+        return "FoundProviders";
+    }
+
+    @GetMapping("providers/rated/{id}")
+    public String ratedProvider(Model model, @PathVariable Integer id) {
+        List<ServiceProvider> serviceProviderList = new ArrayList<>();
+        serviceProviderList.add(serviceProviderService.findById(id));
+                    model
+                .addAttribute("providersByServiceTH",serviceProviderList )
+                .addAttribute("toggleDeactivateEdit", "rated");
         return "FoundProviders";
     }
 }
